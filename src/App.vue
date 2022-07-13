df480680a37d69d43b2d0f5c5de8a268cef590ed
Michael Große <mic.grosse@googlemail.com>
2022-07-13 18:21:23 +0200

    Allow for parsing existing links

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,71 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { ref } from "vue";
 
 const title = ref("");
 const projects = ref("");
 const msg = ref("");
-const link = computed(() => {
-  const base = "https://phabricator.wikimedia.org/maniphest/task/edit/form/1/?";
-  const titleQuery = "title=" + encodeURIComponent(title.value);
-  const projectsQuery = "tags=" + encodeURIComponent(projects.value);
-  const descriptionQuery = "description=" + encodeURIComponent(msg.value);
-  const phabLink =
-    base + titleQuery + "&" + projectsQuery + "&" + descriptionQuery;
+const link = ref("");
 
-  return phabLink;
-});
+const base = "https://phabricator.wikimedia.org/maniphest/task/edit/form/1/?";
+
+// https://phabricator.wikimedia.org/maniphest/task/edit/form/1/?title=Wikidata+Ticket+Template&description=foo&tags=wikidata,accessibility
+
+function parseLink() {
+  let url;
+  try {
+    url = new URL(link.value);
+  } catch (e) {
+    // TODO: show error message
+    console.error(e);
+    throw e;
+  }
+  console.log(url);
+
+  // TODO: more validation
+
+  const params = url.searchParams;
+  title.value = params.get("title") || "";
+  projects.value = params.get("tags") || "";
+  msg.value = params.get("description") || "";
+}
+
+function rebuildLink() {
+  const url = new URL(base);
+  const params = url.searchParams;
+  params.set("title", title.value);
+  params.set("tags", projects.value);
+  params.set("description", msg.value);
+  link.value = url.href;
+}
 </script>
 
 <template>
   <h1>Phabricator Template Builder</h1>
   <form>
     <div>
+      <label for="link">Link (paste existing one here to edit)</label>
+      <input id="link" v-model="link" :placeholder="base" @input="parseLink" />
+      <a :href="link">Open link in phabricator</a>
+    </div>
+
+    <div>
       <label for="title">Title</label>
-      <input id="title" v-model="title" />
+      <input id="title" v-model="title" @input="rebuildLink" />
     </div>
     <div>
       <label for="projects">Projects</label>
-      <input id="projects" v-model="projects" />
+      <input id="projects" v-model="projects" @input="rebuildLink" />
     </div>
     <div>
       <label for="description">Description</label>
-      <textarea id="description" class="description" v-model="msg"></textarea>
+      <textarea
+        id="description"
+        class="description"
+        v-model="msg"
+        @input="rebuildLink"
+      ></textarea>
     </div>
   </form>
-  <a :href="link">open project in phabricator</a>
 </template>
 
 <style>
